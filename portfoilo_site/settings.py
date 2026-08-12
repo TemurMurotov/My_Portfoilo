09f5d01309ddc4c50bfa3f098e46ce9553167772
TemurMurotov <temurmuratov35@gmail.com>
2026-08-13 03:34:06 +0500

    SECRET_KEY environment variable orqali

--- portfoilo_site/settings.py
+++ portfoilo_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2cjb^bt)#va(^6+0&w8+&#qx72_nl!jza0v$!ttvi4&_4hdr(='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-2cjb^bt)#va(^6+0&w8+&#qx72_nl!jza0v$!ttvi4&_4hdr(=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
