7e98f4b7f1243885ddd6c5b6417c2c072f44758b
TemurMurotov <temurmuratov35@gmail.com>
2026-08-13 04:30:55 +0500

    PostgreSQL bazaga ulash

--- portfoilo_site/settings.py
+++ portfoilo_site/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'portfoilo_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
